d697f8588a2a5b34ad60275fa4ced266f6a31137
graytrann <trankhaiphong987@gmail.com>
2023-10-16 10:59:08 +0700

    fix error CORS

--- src/apis/fetcher.js
+++ src/apis/fetcher.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // THƯ VIỆN GỌI API
 // Setup axios instance - tạo ra cấu hình mặc định
 const fetcher = axios.create({
-  baseURL: "http://movienew.cybersoft.edu.vn/api",
+  baseURL: "https://movienew.cybersoft.edu.vn/api",
   headers: {
     TokenCybersoft:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1MiIsIkhldEhhblN0cmluZyI6IjIxLzAyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwODQ3MzYwMDAwMCIsIm5iZiI6MTY4MTE0NjAwMCwiZXhwIjoxNzA4NjIxMjAwfQ.2JFd_iMYjvwU4SaKsLmL_x-kEZcKonddkHVR7z3Gxbc",
