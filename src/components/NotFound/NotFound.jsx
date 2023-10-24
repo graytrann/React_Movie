f33a804ab7cf4a99f03c9025685cf04cbafed4fb
caobaokhanh <caobaokhanh10082002@gmail.com>
2023-10-24 13:57:42 +0700

    Update NotFound Page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 export default function NotFound() {
   // biến điều hướng
   const navigate = useNavigate();
   return (
     <div>
-      <h1>TRANG NOT FOUND</h1>
-      <button onClick={() => navigate("/")}>QUAY VỀ TRANG CHỦ</button>
+      <Header />
+      <Container>
+        <Box
+          sx={{
+            paddingTop: "200px",
+            paddingBottom: "100px",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h1" component="h2">
+            NOT FOUND 404
+          </Typography>
+          <Button
+            sx={{
+              backgroundColor: "rgb(211, 47, 47)",
+              color: "white",
+              "&:hover": {
+                color: "white",
+                backgroundColor: "black",
+              },
+              fontSize: "large",
+              mt: "50px",
+            }}
+            onClick={() => navigate("/")}
+          >
+            QUAY VỀ TRANG CHỦ
+          </Button>
+        </Box>
+      </Container>
+      <Footer />
     </div>
   );
 }
